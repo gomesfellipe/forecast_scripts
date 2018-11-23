279ecf19402bd12d99dfd48fcd1b776b6740aa6a
Fellipe <gomes.fellipe@hotmail.com>
2018-11-23 15:29:48 -0200

    Update forecast.R

--- forecast.R
+++ forecast.R
@@ -1,4 +1,6 @@
-# https://www.datascience.com/blog/decomposition-based-approaches-to-time-series-forecasting?hs_amp=true
+###########################################################################
+# Em construção
+###########################################################################
 
 # - -----------------------------------------------------------------------
 # Forecast 
@@ -8,39 +10,54 @@ library(forecast)
 library(ggplot)
 
 # tema
-theme_set(theme_bw())
+theme_set(theme_bw()+theme(legend.position = "bottom"))
 
 
 # Treino, Teste e Validacao -----------------------------------------------
 
 # Separando dados em test and train 
-train <- window(AirPassengers, end = c(1958, 12))
-test  <- window(AirPassengers, start = c(1959, 1), end = c(1960,12))
+train <- window(AirPassengers, end = c(1959, 12))
+test  <- window(AirPassengers, start = c(1960, 1), end = c(1960,12))
 
 
-# STL ARIMA ---------------------------------------------------------------
+# Base Line ---------------------------------------------------------------
+
+fc_naive     <- naive(train, 12) 
+fc_snaive     <- snaive(train, 12) 
+
+# STL-ARIMA ---------------------------------------------------------------
 
 # Decomposição sazonal e tendência usando LOESS (STL)
-AirPassengersSTL <- stl(AirPassengers, s.window="periodic", robust=TRUE)
-plot(AirPassengersSTL)
+trainSTL <- stl(train, s.window="periodic", robust=TRUE)
+plot(trainSTL)
 
 # Train the STL model, using ARIMA to forecast the remainder 
-AirPassengersSTL_ARIMA <- stlf(train, method="arima")
+trainSTL_ARIMA <- stlf(train, method="arima",h = 12)
 
-
-# LM ----------------------------------------------------------------------
+# TSLM ----------------------------------------------------------------------
 
 # Decomposição sazonal e tendência usando LOESS (STL)
-AirPassengersTSLM <- tslm(train ~ trend + season, data = train)
-fc <- forecast(AirPassengersTSLM, h = 12*2)
+trainTSLM <- tslm(train ~ trend + season, data = train)
+fc <- forecast(trainTSLM, h = 12)
+
+# NNETAR ----------------------------------------------------------------------
+
+trainNNETAR <- nnetar(train)
+fc_NNETAR <- forecast(trainNNETAR, h = 12)
 
 
 # Grafico dos resultados --------------------------------------------------
 
 #Plot dos resultados
 options(repr.plot.width=8, repr.plot.height=4)
-autoplot(train , ylab = 'Passengers') +
+
+g <- 
+  autoplot(AirPassengers , ylab = 'Passengers') +
   scale_x_yearmon() +
-  autolayer(test, series="Test Data") +
-  autolayer(AirPassengersSTL_ARIMA$mean, series="STL + ARIMA")+
-  autolayer(fc$mean, series="LMTS")
+  autolayer(trainSTL_ARIMA$mean, series="STL + ARIMA")+
+  autolayer(fc$mean, series="LMTS") +
+  autolayer(fc_naive$mean, series="Naive") +
+  autolayer(fc_snaive$mean, series="Sazonal Naive") +
+  autolayer(fc_NNETAR$mean, series="NNETAR")
+
+plotly::ggplotly(g)
